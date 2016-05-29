2d4be2a2498272bc4cc985081d7898c4adb55111
Robert D. French <robert@robertdfrench.me>
2016-05-29 17:57:58 -0400

    Refactor so that messages match commands

--- remux.sh
+++ remux.sh
@@ -3,16 +3,33 @@
 # then run `remux sebastion` to establish or reattach
 # a tmux session called `base` on host `sebastion`
 
+_remux_print () {
+  color=$1
+  message=$2
+  printf "\033[$color""m$message\033[0m"
+}
+
+_remux_eval () {
+  blue=34
+  command=$1
+  _remux_print $blue "$command\n"
+  eval $command
+}
+
+_remux_fail () {
+  red=31
+  message=$1
+  _remux_print $red "$message\n"
+}
+
 remux () {
   host=$1
-  printf "\033[34m==> ping $host \033[0m\n"
-  ping -o $host &> /dev/null
+  _remux_eval "ping -o $host &> /dev/null"
   pingable=$?
   if [ $pingable -eq 0 ]
     then
-      printf "\033[34m==> ssh $host 'tmux'\033[0m\n"
-      ssh -t $host 'tmux at -t base || tmux new -s base'
+      _remux_eval "ssh -t $host 'tmux at -t base || tmux new -s base'"
     else
-      printf "\033[31mNot Available :-(\033[0m\n"
+      _remux_fail "$host is not available :-("
   fi
 }
